eb5e3271f47d5a81c524927bc8a826b8e1ce5715
Matt Miguel <75647212+jamiguel23@users.noreply.github.com>
2023-04-07 09:27:07 -0700

    worrking on return

--- csharp-freecodecamp/Program.cs
+++ csharp-freecodecamp/Program.cs
@@ -6,13 +6,17 @@ namespace Csharp
     class Program
     {
         static void Main(string[] args)
-        {   
-            Console.WriteLine("   /|");
-            Console.WriteLine("  / |");
-            Console.WriteLine(" /  |");
-            Console.WriteLine("/___|");
-
+        {
+            Console.WriteLine(cube(3));
+  
             Console.ReadLine();
+        }
+
+        static int cube(int num)
+        {
+            int result = num * num * num;
+
+            return result;
         }
     }
 
