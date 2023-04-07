2e972dd02d3948411e764dc31ade266cdd109229
Matt Miguel <75647212+jamiguel23@users.noreply.github.com>
2023-04-07 08:17:53 -0700

    madlibs complete

--- csharp-freecodecamp/Program.cs
+++ csharp-freecodecamp/Program.cs
@@ -6,11 +6,25 @@ namespace Csharp
     class Program
     {
         static void Main(string[] args)
-        {   
-            Console.WriteLine("   /|");
-            Console.WriteLine("  / |");
-            Console.WriteLine(" /  |");
-            Console.WriteLine("/___|");
+        {
+
+            //int num = Convert.ToInt32("45");
+
+            string color, pluralNoun, celebrity;
+
+            Console.Write("Enter a color: ");
+            color = Console.ReadLine();
+
+            Console.Write("Enter a plural noun: ");
+            pluralNoun = Console.ReadLine();
+
+            Console.Write("Enter a celebrity: ");
+            celebrity = Console.ReadLine();
+
+
+            Console.WriteLine("Roses are " + color);
+            Console.WriteLine( pluralNoun + " are blue");
+            Console.WriteLine("I dont like " + celebrity);
 
             Console.ReadLine();
         }
