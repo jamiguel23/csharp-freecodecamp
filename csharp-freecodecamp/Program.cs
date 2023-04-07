8e86f07ad021385a37aeeac8f360863cdad9ef8c
Matt Miguel <75647212+jamiguel23@users.noreply.github.com>
2023-04-07 10:37:54 -0700

    working calculator

--- csharp-freecodecamp/Program.cs
+++ csharp-freecodecamp/Program.cs
@@ -1,19 +1,52 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using System.Runtime.Intrinsics.X86;
+
 namespace Csharp
 {
 
     class Program
     {
         static void Main(string[] args)
-        {   
-            Console.WriteLine("   /|");
-            Console.WriteLine("  / |");
-            Console.WriteLine(" /  |");
-            Console.WriteLine("/___|");
+        {
+
+            Console.Write("enter an number: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter Operator: ");
+            string op = Console.ReadLine();
+
+            Console.Write("enter another number: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            if(op == "+")
+            {
+                Console.Write(num1 + num2);
+            }
+            else if (op == "-")
+            {
+                Console.Write(num1 - num2);
+
+            }
+            else if (op == "/")
+            {
+                Console.Write(num1 / num2);
+
+            }
+            else if (op == "*")
+            {
+                Console.Write(num1 * num2);
+
+            }
+            else
+            {
+                Console.WriteLine("Invalid Operator");
+            }
 
             Console.ReadLine();
         }
+
+
     }
 
 }
