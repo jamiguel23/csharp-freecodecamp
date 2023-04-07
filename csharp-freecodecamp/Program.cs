87f3433491b40019b127d98d95e7ecdb51174c1e
Matt Miguel <75647212+jamiguel23@users.noreply.github.com>
2023-04-07 08:28:58 -0700

    working on int and string arr

--- csharp-freecodecamp/Program.cs
+++ csharp-freecodecamp/Program.cs
@@ -6,11 +6,14 @@ namespace Csharp
     class Program
     {
         static void Main(string[] args)
-        {   
-            Console.WriteLine("   /|");
-            Console.WriteLine("  / |");
-            Console.WriteLine(" /  |");
-            Console.WriteLine("/___|");
+        {
+
+            int[] luckyNumber = { 4, 8, 15, 1, 23, 42 };
+            string[] people = new string[10];// if starting new empty string array use new string[i] where i is the amount of items in the string
+
+            people[0] = "Phil";
+            luckyNumber[1] = 640;
+            Console.WriteLine(people[0]);
 
             Console.ReadLine();
         }
