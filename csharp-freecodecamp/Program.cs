6f5ce53762673e755848e25116d14228a8ca574e
Matt Miguel <75647212+jamiguel23@users.noreply.github.com>
2023-04-07 09:21:28 -0700

    added method work

--- csharp-freecodecamp/Program.cs
+++ csharp-freecodecamp/Program.cs
@@ -6,13 +6,19 @@ namespace Csharp
     class Program
     {
         static void Main(string[] args)
-        {   
-            Console.WriteLine("   /|");
-            Console.WriteLine("  / |");
-            Console.WriteLine(" /  |");
-            Console.WriteLine("/___|");
-
+        {
+            Console.WriteLine("insert your name ");
+            string userName = Console.ReadLine();
+            Console.WriteLine("insert your age ");
+            int age = Convert.ToInt32(Console.ReadLine());
+            SayHi(userName, age);
             Console.ReadLine();
+        }
+
+        static void SayHi(string name, int age)
+        {
+            Console.WriteLine("hello " + name + " your age is " + age);
+
         }
     }
 
