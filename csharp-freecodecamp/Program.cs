205f984fdad1add4de28c42208a56569a0d46915
Matt Miguel <75647212+jamiguel23@users.noreply.github.com>
2023-04-07 10:26:12 -0700

    getmax methiod

--- csharp-freecodecamp/Program.cs
+++ csharp-freecodecamp/Program.cs
@@ -6,13 +6,35 @@ namespace Csharp
     class Program
     {
         static void Main(string[] args)
-        {   
-            Console.WriteLine("   /|");
-            Console.WriteLine("  / |");
-            Console.WriteLine(" /  |");
-            Console.WriteLine("/___|");
+        {
+            Console.WriteLine(Convert.ToInt32(GetMax(10, 30, 60)));
 
+            
             Console.ReadLine();
+        }
+
+        static int GetMax(int a, int b, int c)
+        {
+            //if( a == b) {
+            //    {
+            //        int[] result = { a, b };
+            //        return result
+            //     }
+
+            if(a >= b && a>= c) 
+            {
+       
+                return a;
+
+            }
+            else if(b>= a && b>=c)
+            {
+                return b;
+            }
+            else
+            {
+                return c;
+            }
         }
     }
 
